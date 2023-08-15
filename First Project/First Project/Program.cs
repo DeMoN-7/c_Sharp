ece5c5506787e9ef8bba5796305a021d59f038d1
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2023-08-15 16:20:33 +0530

    commit

---
+++ First Project/First Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jai Shree Ram!!!!!");
+            Console.ReadLine();
+        }
+    }
+}
