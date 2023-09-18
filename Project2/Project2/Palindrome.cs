92153ef8b32f4469e370dff8c0dc2e02ad5f427d
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2023-09-18 13:05:04 +0530

    commit

---
+++ Project2/Project2/Palindrome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+     class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Project2/Project2/temp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class temp
+    {
+        static void Main(string[] args)
+        {
+            string s= "ABCDCBA";
+            char[] chars = s.ToCharArray();
+            int x = chars.Length-1;
+            int k = 0;
+            for (int i = 0; i <=x/2; i++)
+            {
+                if (!(chars[i] == chars[x - i]))
+                {
+                    k = 1;
+                    Console.WriteLine("not pallindrome");
+                    break;
+                }
+            }
+            if (k == 0)
+            {
+                Console.WriteLine("pallindrome");
+            }
+
+        }
+    }
+}
