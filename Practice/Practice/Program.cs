2ed66a8e6db7c6b483c352729f0e87b6aa98b8b4
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2023-09-27 00:05:00 +0530

    commit

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age,roll;
+            string name;
+            age = Convert.ToInt32(Console.ReadLine());
+            roll = Convert.ToInt32(Console.ReadLine());
+            name = Console.ReadLine();
+            Console.WriteLine("Name"+name);
+            Console.WriteLine("Age"+age);
+            Console.WriteLine("Roll"+roll);
+            name = Console.ReadLine();
+
+
+
+        }
+    }
+}
