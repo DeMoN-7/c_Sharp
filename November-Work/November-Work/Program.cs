846832a586b8d28eda67f050337a43ec4d5ad3fa
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2023-12-10 00:28:54 +0530

    commit

---
+++ November-Work/November-Work/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace November_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* string[] arr = { "Aman", "Chaman", "Ayush", "Coomshal" };
+             foreach (string c in arr)
+             { 
+                 Console.WriteLine(c.Length +" "+ c[c.Length - 1]);
+             }
+             Console.ReadKey();
+            */
+            int[] arr = { 7, 5, 5, 2, 7, 2, 5 };
+            int n = 0;
+            foreach(int x in arr)
+            {
+                n = n ^ x;
+            }
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Term-Work/Term-Work/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Palindrome
+{
+    static void Main(string[] args)
+    {
+        int num = 0;
+        int rev = 0;
+        int rem = 0;
+        int temp = 0;
+        Console.WriteLine("Enter number:");
+        num = int.Parse(Console.ReadLine());
+        temp = num;
+        while (num > 0)
+        {
+            rev = (rev * 10) + (num % 10);
+            num = num / 10;
+        }
+        if (temp == rev)
+            Console.WriteLine("Number is Palindrome");
+        else
+            Console.WriteLine("Number is not Palindrome");
+        Console.ReadLine();
+    }
+}
